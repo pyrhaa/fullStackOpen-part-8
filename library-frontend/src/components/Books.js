4d3468d64cec984d45328e90c638702f331b13f3
pyrhaa <sboubtane@protonmail.com>
2022-08-20 12:14:07 +0200

    8.19 ok

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,4 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
 const Books = ({ show, resultBooks }) => {
+  const [books, setBooks] = useState([]);
+  const [filteredBooks, setFilteredBooks] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState('');
+
+  useEffect(() => {
+    if (resultBooks.data) {
+      const allBooks = resultBooks.data.allBooks || [];
+      setBooks(allBooks);
+      let genres = ['All genres'];
+      allBooks.forEach((el) => {
+        el.genres.forEach((gr) => {
+          if (genres.indexOf(gr) === -1) {
+            genres.push(gr);
+          }
+        });
+      });
+      setGenres(genres);
+      setSelectedGenre('All genres');
+    }
+  }, [resultBooks]);
+
+  useEffect(() => {
+    if (selectedGenre === 'All genres') {
+      setFilteredBooks(books);
+    } else {
+      setFilteredBooks(
+        books.filter((book) => book.genres.indexOf(selectedGenre) !== -1)
+      );
+    }
+  }, [books, selectedGenre]);
+
   if (!show) {
     return null;
   }
@@ -6,8 +40,6 @@ const Books = ({ show, resultBooks }) => {
   if (resultBooks.loading) {
     return <div>loading...</div>;
   }
-
-  const books = resultBooks.data.allBooks || [];
 
   return (
     <div>
@@ -20,7 +52,7 @@ const Books = ({ show, resultBooks }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -29,6 +61,14 @@ const Books = ({ show, resultBooks }) => {
           ))}
         </tbody>
       </table>
+      <div>
+        {genres.length > 0 &&
+          genres.map((genre) => (
+            <button onClick={() => setSelectedGenre(genre)} key={genre}>
+              {genre}
+            </button>
+          ))}
+      </div>
     </div>
   );
 };
